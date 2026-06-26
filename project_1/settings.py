46dcb78e7566527e5853976252786969d9c0182d
Ajay md <ajay@gmail.com>
2026-06-26 16:04:10 +0530

    add requirements

--- project_1/settings.py
+++ project_1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)2vc4fu$a*1r77z43o2_ho^@cxr55iygvgaiklbp=ij73@-%fy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,6 +116,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# Media Files (For user/admin uploaded product images)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
